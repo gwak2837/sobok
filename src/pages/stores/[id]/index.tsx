d2e25c2f0f6ea954c26c74d5dfc125bb6ecbb237
Taeuk Gwak <gwak2837@kakao.com>
2021-08-26 16:33:36 +0900

    Add store layout on store detail page

--- src/pages/stores/[id]/index.tsx
+++ src/pages/stores/[id]/index.tsx
@@ -1,4 +1,14 @@
+import type { ReactElement, ReactNode } from 'react'
 import PageHead from 'src/components/PageHead'
+
+export function StoreLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <div>매장 레이아웃</div>
+      {children}
+    </div>
+  )
+}
 
 const description = ''
 
@@ -8,4 +18,8 @@ export default function StorePage() {
       매장 페이지
     </PageHead>
   )
+}
+
+StorePage.getLayout = function getLayout(page: ReactElement) {
+  return <StoreLayout>{page}</StoreLayout>
 }
