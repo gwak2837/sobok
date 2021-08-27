462be3873775380b4a5aaf9947b7fb39d64ffca2
HyojinKim <gywls00100@gmail.com>
2021-08-27 18:06:53 +0900

    Make flex container templates

---
+++ src/styles/FlexContainer.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const FlexContainerCenterCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const FlexContainerAlignCenter = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const FlexContainerJustifyCenter = styled.div`
+  display: flex;
+  justify-content: center;
+`
+export const FlexContainerBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const FlexContainerAround = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
