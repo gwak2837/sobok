14ca1d0d2b1d84cca576c7e562b82bb1ca7a2a6c
HyojinKim <gywls00100@gmail.com>
2021-08-27 20:21:13 +0900

    Add TopHeader to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import PageHead from 'src/components/PageHead'
+import TopHeader from 'src/components/TopHeader'
+import NavigationLayout from 'src/layouts/NavigationLayout'
 import styled from 'styled-components'
+import { IconImg } from 'src/components/atoms/Styles'
 
+const LocalNav = styled(TopHeader)`
+  justify-content: space-between;
+  //padding: 0 1rem;
+`
+const LocationDiv = styled.div`
+  background-color: antiquewhite;
+  //margin-right: auto;
+`
+const TopIconDiv = styled.div`
+  background-color: #e2caab;
+`
+
+const LocationText = styled.div`
+  font-size: 1.3rem;
+`
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
@@ -25,21 +43,39 @@ const SquareFrame = styled.div`
 export default function HomePage() {
   return (
     <PageHead>
-      <div>
-        <Link href="/@userId1">사용자 페이지</Link>
-      </div>
-      <div>
-        <Link href="/@userId2">사용자2 페이지</Link>
-      </div>
-      <div>
-        <Link href="/register">회원가입 페이지</Link>
-      </div>
-      <div>
-        <Link href="/login">로그인 페이지</Link>
-      </div>
-      <div>
-        <Link href="/stores/1">매장 페이지</Link>
-      </div>
+      <NavigationLayout>
+        <LocalNav>
+          <LocationDiv>
+            <LocationText>흑석동</LocationText>
+            <Link href="/location">
+              <LocationText>{'>'}</LocationText>
+            </Link>
+          </LocationDiv>
+          <TopIconDiv>
+            <Link href="/map">
+              <IconImg src="/598@3x.png" alt="map" layout="fill" />
+            </Link>
+            <Link href="/search">
+              <IconImg src="/search@3x.png" alt="map" layout="fill" />
+            </Link>
+          </TopIconDiv>
+        </LocalNav>
+        <div>
+          <Link href="/@userId1">사용자 페이지</Link>
+        </div>
+        <div>
+          <Link href="/@userId2">사용자2 페이지</Link>
+        </div>
+        <div>
+          <Link href="/register">회원가입 페이지</Link>
+        </div>
+        <div>
+          <Link href="/login">로그인 페이지</Link>
+        </div>
+        <div>
+          <Link href="/stores/1">매장 페이지</Link>
+        </div>
+      </NavigationLayout>
     </PageHead>
   )
 }
