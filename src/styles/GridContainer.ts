15f4cbb5bbfcc482bd2ca9e29e11bf4fa71a39e6
HyojinKim <gywls00100@gmail.com>
2021-08-27 18:08:05 +0900

    Make grid container templates

---
+++ src/styles/GridContainer.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const GridContainerGap = styled.div`
+  display: grid;
+  gap: 1rem;
+`
+export const GridContainer = styled.div`
+  display: grid;
+`
