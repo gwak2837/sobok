ac2ac4835ab8288b278d559dd5dce57db22e6b95
Taeuk Gwak <gwak2837@kakao.com>
2021-08-28 15:00:27 +0900

    Test adding dynamically color on SVG file

--- src/layouts/NavigationLayout.tsx
+++ src/layouts/NavigationLayout.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 import ClientSideLink from 'src/components/atoms/ClientSideLink'
-import { SOBOK_COLOR, LIGHT_GREY } from 'src/models/constants'
+import { News, Trend } from 'src/components/atoms/SVGs'
+import { SOBOK_COLOR, SOBOK_ACHROMATIC_COLOR } from 'src/models/constants'
 import { TABLET_MIN_WIDTH } from 'src/utils/constants'
 import styled from 'styled-components'
 
@@ -39,7 +40,7 @@ const TextDiv = styled.div`
 `
 
 const SelectedStyle = { color: SOBOK_COLOR }
-const UnSelectedStyle = { color: LIGHT_GREY }
+const UnSelectedStyle = { color: SOBOK_ACHROMATIC_COLOR }
 
 type Props = {
   children: ReactNode
@@ -50,51 +51,39 @@ export default function NavigationLayout({ children }: Props) {
 
   const userId = 'userId1'
 
+  console.log(asPath)
+
   return (
     <>
       {children}
       <FixedHeader>
         <NaigationContainer>
           <ClientSideLink href="/news">
-            <IconDiv
-              style={asPath === '/news' || asPath === '/news/all' ? SelectedStyle : UnSelectedStyle}
-            >
-              <Image src="/images/news.svg" alt="news.svg" width={18} height={18} />
-            </IconDiv>
+            <News color={asPath === '/news' || asPath === '/news/all' ? SOBOK_COLOR : undefined} />
             <TextDiv>소식</TextDiv>
           </ClientSideLink>
         </NaigationContainer>
         <NaigationContainer>
           <ClientSideLink href="/trends">
-            <IconDiv style={asPath === '/trends' ? SelectedStyle : UnSelectedStyle}>
-              <Image src="/images/trend.svg" alt="trend.svg" width={17} height={19} />
-            </IconDiv>
+            <Trend color={asPath === '/trends' ? SOBOK_COLOR : undefined} />
             <TextDiv>트렌드</TextDiv>
           </ClientSideLink>
         </NaigationContainer>
         <NaigationContainer>
           <ClientSideLink href="/">
-            <IconDiv style={asPath === '/' ? SelectedStyle : UnSelectedStyle}>
-              <Image src="/images/home.svg" alt="home.svg" width={24} height={21} />
-            </IconDiv>
+            <Image src="/images/home.svg" alt="home" width={24} height={21} />
             <TextDiv>홈</TextDiv>
           </ClientSideLink>
         </NaigationContainer>
         <NaigationContainer>
           <ClientSideLink href={`/@${userId}/buckets`}>
-            <IconDiv
-              style={asPath.startsWith(`/@${userId}/buckets`) ? SelectedStyle : UnSelectedStyle}
-            >
-              <Image src="/images/bucket.svg" alt="bucket.svg" width={18} height={16} />
-            </IconDiv>
+            <Image src="/images/bucket.svg" alt="bucket" width={18} height={16} />
             <TextDiv>버킷</TextDiv>
           </ClientSideLink>
         </NaigationContainer>
         <NaigationContainer>
           <ClientSideLink href={`/@${userId}`}>
-            <IconDiv style={asPath === `/@${userId}` ? SelectedStyle : UnSelectedStyle}>
-              <Image src="/images/my.svg" alt="my.svg" width={16} height={19} />
-            </IconDiv>
+            <Image src="/images/my.svg" alt="my" width={16} height={19} />
             <TextDiv>MY</TextDiv>
           </ClientSideLink>
         </NaigationContainer>
