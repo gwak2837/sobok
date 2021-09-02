ca16a197c328828ea90c9848ab9f240bf4f90a60
HyojinKim <gywls00100@gmail.com>
2021-09-03 03:55:53 +0900

    Modify card layout

--- src/components/StorePlaceCard.tsx
+++ src/components/StorePlaceCard.tsx
@@ -1,35 +1,50 @@
 import Image from 'next/image'
 import styled from 'styled-components'
-import { useRouter } from 'next/router'
-import type { StoreMenusQuery } from 'src/graphql/generated/types-and-hooks'
-import type { ArrayElement } from 'src/utils/types'
-
-type Props = {
-  storeMenu: ArrayElement<StoreMenusQuery['menus2']>
-}
 
 const StoreContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-auto-rows: minmax(calc(200px + 10vw), auto);
-  column-gap: 0.8rem;
+  padding: 1rem;
+  gap: 0.8rem;
+  background-color: #f7f7f7;
 `
 
 const StoreCard = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #c4c4c4;
 `
 
-function StorePlaceCard({ storeMenu }: Props) {
-  const router = useRouter()
-
-  function goToStoreMenuPage() {
-    router.push(`${router.asPath}/${storeMenu.name}`)
-  }
-
+function StorePlaceCard() {
   return (
     <StoreContainer>
-      <StoreCard></StoreCard>
+      <StoreCard>
+        <div>이미지</div>
+        <div>매장명</div>
+        <div>카테고리</div>
+        <div>태그</div>
+      </StoreCard>
+      <StoreCard>
+        <div>이미지</div>
+        <div>매장명</div>
+        <div>카테고리</div>
+        <div>태그</div>
+      </StoreCard>
+      <StoreCard>
+        <div>이미지</div>
+        <div>매장명</div>
+        <div>카테고리</div>
+        <div>태그</div>
+      </StoreCard>
+      <StoreCard>
+        <div>이미지</div>
+        <div>매장명</div>
+        <div>카테고리</div>
+        <div>태그</div>
+      </StoreCard>
     </StoreContainer>
   )
 }
