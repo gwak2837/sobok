998719e74dd3207fa096597aab6800abdcbe7479
HyojinKim <gywls00100@gmail.com>
2021-09-04 00:41:14 +0900

    Add StoreMenuCard

--- src/pages/menus.tsx
+++ src/pages/menus.tsx
@@ -6,6 +6,7 @@ import PageHead from 'src/components/PageHead'
 import HomeLayout, { HomeContext } from 'src/layouts/HomeLayout'
 import NavigationLayout from 'src/layouts/NavigationLayout'
 import MenuCategory from 'src/components/MenuCategory'
+import StoreMenuCard from 'src/components/StoreMenuCard'
 
 const description = ''
 
@@ -15,6 +16,7 @@ export default function MenusPage() {
   return (
     <PageHead title={`${townName} 메뉴 - 소복`} description={description}>
       <MenuCategory />
+      <StoreMenuCard />
       <div>메뉴 페이지</div>
     </PageHead>
   )
