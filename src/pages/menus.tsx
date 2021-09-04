02422c734c1caf829a1b48f1251aeb5dedf7ca14
Taeuk Gwak <gwak2837@kakao.com>
2021-09-04 15:01:29 +0900

    Fetch menu data from api

--- src/pages/menus.tsx
+++ src/pages/menus.tsx
@@ -1,21 +1,33 @@
 import { ReactElement, useContext } from 'react'
 import styled from 'styled-components'
-import Link from 'next/link'
-import Image from 'next/image'
 import PageHead from 'src/components/PageHead'
 import HomeLayout, { HomeContext } from 'src/layouts/HomeLayout'
 import NavigationLayout from 'src/layouts/NavigationLayout'
 import MenuCategory from 'src/components/MenuCategory'
+import { useMenusQuery } from 'src/graphql/generated/types-and-hooks'
+import MenuCard from 'src/components/MenuCard'
 
 const description = ''
 
 export default function MenusPage() {
   const { townName } = useContext(HomeContext)
 
+  const { data, loading } = useMenusQuery({ skip: !townName, variables: { town: townName } })
+
+  const menus = data?.menusByTownAndCategory
+
   return (
     <PageHead title={`${townName} 메뉴 - 소복`} description={description}>
       <MenuCategory />
-      <div>메뉴 페이지</div>
+      {loading || !menus ? (
+        'loading'
+      ) : (
+        <ul>
+          {menus.map((menu) => (
+            <MenuCard key={menu.id} menu={menu} />
+          ))}
+        </ul>
+      )}
     </PageHead>
   )
 }
