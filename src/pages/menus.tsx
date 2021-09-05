121414e61622641ddb7041ee951ebef0c1c8ec85
HyojinKim <gywls00100@gmail.com>
2021-09-05 23:27:34 +0900

    Add filter button

--- src/pages/menus.tsx
+++ src/pages/menus.tsx
@@ -9,6 +9,22 @@ import MenuCard from 'src/components/MenuCard'
 
 const description = ''
 
+const MenuContainer = styled.div`
+  background-color: #fcfcfc;
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+`
+
+const FilterButton = styled.button`
+  width: 5rem;
+  height: 2.2rem;
+  border-radius: 0.3em;
+  border: solid 1px #e8e8e8;
+  background-color: white;
+  margin-bottom: 1rem;
+`
+
 export default function MenusPage() {
   const { townName } = useContext(HomeContext)
 
@@ -19,15 +35,18 @@ export default function MenusPage() {
   return (
     <PageHead title={`${townName} 메뉴 - 소복`} description={description}>
       <MenuCategory />
-      {loading || !menus ? (
-        'loading'
-      ) : (
-        <ul>
-          {menus.map((menu) => (
-            <MenuCard key={menu.id} menu={menu} />
-          ))}
-        </ul>
-      )}
+      <MenuContainer>
+        <FilterButton>거리순</FilterButton>
+        {loading || !menus ? (
+          'loading'
+        ) : (
+          <ul>
+            {menus.map((menu) => (
+              <MenuCard key={menu.id} menu={menu} />
+            ))}
+          </ul>
+        )}
+      </MenuContainer>
     </PageHead>
   )
 }
