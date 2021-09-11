ef6a707be74c4d1ef7e18c0c46e7b7f9f666a535
Taeuk Gwak <gwak2837@kakao.com>
2021-09-11 15:26:27 +0900

    Fetch contents in buckets

--- src/pages/[userUniqueName]/store-buckets/[id].tsx
+++ src/pages/[userUniqueName]/store-buckets/[id].tsx
@@ -1,18 +1,34 @@
 import { useRouter } from 'next/router'
 import { ReactElement } from 'react'
 import PageHead from 'src/components/PageHead'
+import StoreCard from 'src/components/StoreCard'
+import { useBucketStoresQuery } from 'src/graphql/generated/types-and-hooks'
 import NavigationLayout from 'src/layouts/NavigationLayout'
+import { getUserUniqueName } from 'src/utils/commons'
 
 const description = ''
 
 export default function UserBucketPage() {
   const router = useRouter()
-
   const bucketId = (router.query.id ?? '') as string
+  const userUniqueName = getUserUniqueName(router)
+
+  const { data, loading, error } = useBucketStoresQuery({
+    skip: !bucketId || !userUniqueName,
+    variables: { bucketId, userUniqueName },
+  })
+
+  const stores = data?.storesInBucket
 
   return (
     <PageHead title=" - 소복" description={description}>
-      사용자 버킷 상세 페이지
+      {loading ? (
+        <div>loading</div>
+      ) : stores ? (
+        stores.map((store) => <StoreCard key={store.id} store={store} />)
+      ) : (
+        '텅...'
+      )}
     </PageHead>
   )
 }
