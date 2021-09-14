cccf244a8198048889e2102326ca8306b6bf732b
Taeuk Gwak <gwak2837@kakao.com>
2021-09-14 20:15:11 +0900

    Update useQueryString.ts

--- src/hooks/useQueryString.ts
+++ src/hooks/useQueryString.ts
@@ -1,10 +1,19 @@
 import { useEffect, useState } from 'react'
 
+export function getJSONFromQueryString(queryString: string) {
+  return JSON.parse(
+    '{"' + queryString.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
+    (key, value) => {
+      return key === '' ? value : decodeURIComponent(value)
+    }
+  )
+}
+
 export default function useQueryString() {
-  const [queryString, setQueryString] = useState('')
+  const [queryString, setQueryString] = useState<Record<string, any> | null>(null)
 
   useEffect(() => {
-    setQueryString(window.location.search.substr(1))
+    setQueryString(getJSONFromQueryString(location.search.substr(1)))
   }, [])
 
   return queryString
