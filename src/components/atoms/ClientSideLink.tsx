32ce71509ec2f6c680c3e895b2164ce85afb265a
Taeuk Gwak <gwak2837@kakao.com>
2021-09-14 16:42:28 +0900

    Add svg components

--- src/components/atoms/ClientSideLink.tsx
+++ src/components/atoms/ClientSideLink.tsx
@@ -5,13 +5,13 @@ import { stopPropagation } from 'src/utils/commons'
 type Props = {
   children: ReactNode
   href: string
-  style?: CSSProperties
+  className?: string
 }
 
-function ClientSideLink({ children, href, style }: Props) {
+function ClientSideLink({ children, href, className }: Props) {
   return (
     <Link href={href}>
-      <a href={href} onClick={stopPropagation} style={style}>
+      <a className={className} href={href} onClick={stopPropagation}>
         {children}
       </a>
     </Link>
