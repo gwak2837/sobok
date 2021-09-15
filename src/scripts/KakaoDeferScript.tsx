1d6734a57e6513d867ea2012c95576097b12f2f6
Taeuk Gwak <gwak2837@kakao.com>
2021-09-15 19:19:56 +0900

    Fix runtime error

--- src/scripts/KakaoDeferScript.tsx
+++ src/scripts/KakaoDeferScript.tsx
@@ -1,6 +1,12 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 
 function KakaoDeferScript() {
+  // Kakao API 초기화
+  useEffect(() => {
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY)
+  }, [])
+
   return (
     <Head>
       <script defer src="https://developers.kakao.com/sdk/js/kakao.min.js" />
