317a9e5d4dcbc4eaebf202b72329055046d2e9f7
Taeuk Gwak <gwak2837@kakao.com>
2021-09-17 19:51:03 +0900

    Modify style

--- src/pages/location.tsx
+++ src/pages/location.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script'
 import { useRef } from 'react'
 import PageHead from 'src/components/PageHead'
 import { useRecoilState } from 'recoil'
@@ -6,13 +5,14 @@ import { currentLocation } from 'src/models/recoil'
 import { toast } from 'react-toastify'
 import { getCurrentPositionFromGeolocationAPI } from 'src/utils/web-api'
 import { createNaverMap } from 'src/utils/commons'
+import NaverMapScript from 'src/scripts/NaverMapScript'
 
 const description = ''
 
 export default function LocationPage() {
-  const mapRef = useRef<any>()
+  const naverMapRef = useRef<any>()
 
-  const [coordinates, setCoordinates] = useRecoilState(currentLocation)
+  const [, setCoordinates] = useRecoilState(currentLocation)
 
   async function initializeNaverMap() {
     const position = await getCurrentPositionFromGeolocationAPI().catch((error) => {
@@ -22,17 +22,21 @@ export default function LocationPage() {
 
     const coords = position?.coords
 
-    mapRef.current = createNaverMap(coords?.latitude ?? 37.3595704, coords?.longitude ?? 127.105399)
+    if (coords) {
+      setCoordinates(coords)
+    }
+
+    naverMapRef.current = createNaverMap(
+      coords?.latitude ?? 37.3595704,
+      coords?.longitude ?? 127.105399
+    )
   }
 
   return (
     <PageHead title=" - 소복" description={description}>
-      <Script
-        onLoad={initializeNaverMap}
-        src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=uprwl7nxp3"
-      />
+      <NaverMapScript onLoad={initializeNaverMap} />
 
-      <div id="map" ref={mapRef} style={{ width: '100vw', height: '100vh' }}></div>
+      <div id="map" ref={naverMapRef} style={{ width: '100vw', height: '100vh' }}></div>
     </PageHead>
   )
 }
