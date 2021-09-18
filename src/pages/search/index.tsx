617c8a6283b05d7fb7a1410c233c04660a1852d8
HyojinKim <gywls00100@gmail.com>
2021-09-18 16:41:40 +0900

    Add search form layout

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,13 @@
+import type { ReactElement, ReactNode } from 'react'
 import PageHead from 'src/components/PageHead'
+import SearchFormLayout from 'src/layouts/SearchFormLayout'
 
 const description = ''
 
-export default function SearchPage() {
-  return (
-    <PageHead title=" - 소복" description={description}>
-      검색 페이지
-    </PageHead>
-  )
+export default function SearchPage({ children }: { children: ReactNode }) {
+  return <PageHead title=" - 소복" description={description}>{}</PageHead>
+}
+
+SearchPage.getLayout = function getLayout(page: ReactElement) {
+  return <SearchFormLayout>{page}</SearchFormLayout>
 }
