9bfb5468acaee4a63ed4a036080a35053f6f8b79
HyojinKim <gywls00100@gmail.com>
2021-09-18 18:21:28 +0900

    Make container

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,13 +1,34 @@
 import type { ReactElement, ReactNode } from 'react'
 import PageHead from 'src/components/PageHead'
 import SearchFormLayout from 'src/layouts/SearchFormLayout'
+import styled from 'styled-components'
 
 const description = ''
+
+const HistoryContainer = styled.div`
+  padding: 1rem;
+  height: 15rem;
+`
+const PopularContainer = styled.div`
+  padding: 1rem;
+`
+
+const HorizontalLine = styled.div`
+  height: 8px;
+  background-color: #eaeaea;
+  opacity: 0.58;
+`
 
 export default function SearchPage() {
   return (
     <PageHead title=" - 소복" description={description}>
-      {}
+      <HistoryContainer>
+        <h3>최근검색어</h3>
+      </HistoryContainer>
+      <HorizontalLine />
+      <PopularContainer>
+        <h3>인기검색어</h3>
+      </PopularContainer>
     </PageHead>
   )
 }
