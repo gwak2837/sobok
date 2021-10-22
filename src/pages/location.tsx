cbf064ed21f07471a19ba7e89da6272bbd26f88c
Taeuk Gwak <gwak2837@kakao.com>
2021-10-22 19:41:31 +0900

    Add loading state on location page

--- src/pages/location.tsx
+++ src/pages/location.tsx
@@ -1,20 +1,23 @@
-import { useRef } from 'react'
-import PageHead from 'src/components/PageHead'
-import { useRecoilState } from 'recoil'
-import { currentLocation } from 'src/models/recoil'
+import { useRef, useState } from 'react'
 import { toast } from 'react-toastify'
-import { getCurrentPositionFromGeolocationAPI } from 'src/utils/web-api'
-import { createNaverMap } from 'src/utils/commons'
+import { useRecoilState } from 'recoil'
+import PageHead from 'src/components/PageHead'
+import { currentLocation } from 'src/models/recoil'
 import NaverMapScript from 'src/scripts/NaverMapScript'
+import { createNaverMap } from 'src/utils'
+import { getCurrentPositionFromGeolocationAPI } from 'src/utils/web-api'
 
 const description = ''
 
 export default function LocationPage() {
+  const [naverMapLoading, setNaverMapLoading] = useState(true)
   const naverMapRef = useRef<any>()
 
   const [, setCoordinates] = useRecoilState(currentLocation)
 
   async function initializeNaverMap() {
+    setNaverMapLoading(false)
+
     const position = await getCurrentPositionFromGeolocationAPI().catch((error) => {
       toast.warn(error)
       return null
@@ -35,6 +38,8 @@ export default function LocationPage() {
   return (
     <PageHead title=" - 소복" description={description}>
       <NaverMapScript onLoad={initializeNaverMap} />
+
+      {naverMapLoading && <div>loading</div>}
 
       <div id="map" ref={naverMapRef} style={{ width: '100%', height: '100vh' }} />
     </PageHead>
