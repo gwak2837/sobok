756bbf50923c77194756b63fb53134bcd71d6644
Taeuk Gwak <gwak2837@kakao.com>
2021-10-23 23:48:48 +0900

    Add custom infinite scroll

--- src/apollo/cache.ts
+++ src/apollo/cache.ts
@@ -3,16 +3,20 @@ import { InMemoryCache } from '@apollo/client'
 const cache = new InMemoryCache({
   typePolicies: {
     Query: {
-      fields: {},
+      fields: {
+        storesByTownAndCategories: {
+          merge(existing, incoming) {
+            if (!existing) {
+              return incoming
+            } else {
+              return [...existing, ...incoming]
+            }
+          },
+          keyArgs: ['town', 'categories', 'order'],
+        },
+      },
     },
   },
 })
-
-// 지금 persistCache가 초반에 적용이 안 될 수 있다는 점이 있는데 나중에 top-level await 적용하기
-// 페이지를 새로고침할 때 Apollo 캐시를 유지시키기 위해서 sessionStorage에 저장
-// persistCache({
-//   cache,
-//   storage: new SessionStorageWrapper(typeof window !== 'undefined' ? sessionStorage : null),
-// })
 
 export default cache
