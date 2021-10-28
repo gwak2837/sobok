af8d13e0d5f66cd785dea265a0e6922900990abd
Taeuk Gwak <gwak2837@kakao.com>
2021-10-28 21:52:40 +0900

    Update store card styles

--- src/components/StoreCard.tsx
+++ src/components/StoreCard.tsx
@@ -1,89 +1,138 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { StoreCardFragment } from 'src/graphql/generated/types-and-hooks'
+import React, { MouseEvent as ReactMouseEvent, useEffect } from 'react'
+import { toastApolloError } from 'src/apollo/error'
+import {
+  StoreCardFragment,
+  useToggleLikedStoreMutation,
+} from 'src/graphql/generated/types-and-hooks'
+import { Skeleton } from 'src/styles'
+import HeartFilledIcon from 'src/svgs/HeartFilledIcon'
+import LocationIcon from 'src/svgs/location.svg'
 import { distanceBetween, formatDistance } from 'src/utils'
 import styled from 'styled-components'
 
 import Loading from './Loading'
 
-const FlexContainerLi = styled.li`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  border-radius: 0.4rem;
-  overflow: hidden;
+const GridContainerGap = styled.li`
+  display: grid;
+  gap: 0.4rem;
 `
 
-const CardImage = styled.div`
+const Frame16to10 = styled.div`
   position: relative;
-  height: 7.5em;
-  padding-bottom: 15.63%;
-  border-radius: 0.4rem;
+  padding-top: 62.5%;
+  border-radius: 4px;
   overflow: hidden;
 `
 
-const CardInfo = styled.div`
+const FlexContainerBetweenCenter = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 0.2rem;
-  /* background-color: #eaf7e7; */
+  align-items: center;
 `
 
-const CardCategory = styled.div`
-  flex-wrap: wrap;
-  padding: 0.2rem;
+const FlexContainerCenter = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const FlexContainerGap = styled.div`
+  display: flex;
+  gap: 0.3rem;
+  flex: 0 0 auto;
+
+  overflow: scroll hidden;
+  scrollbar-width: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `
 
 const CategoryButton = styled.button`
-  padding: 0.4rem 0.5rem;
-  text-align: center;
-  color: white;
-  font-size: 0.8rem;
-  border-radius: 3rem;
+  background: #ff9f74;
   border: none;
-  margin-right: 0.2rem;
-  margin-bottom: 0.2rem;
-  background-color: #ff9f74;
-`
-
-const CardTags = styled.div`
-  padding: 0.2rem;
-  //background-color: #eaf7e7;
+  border-radius: 9999px;
+  color: white;
+  font-size: 0.85rem;
+  padding: 0.4rem 0.7rem 0.5rem;
+  text-align: center;
+  white-space: nowrap; 
 `
 
 const Tag = styled.span`
   flex-wrap: nowrap;
   color: grey;
-  font-size: 0.8rem;
+  font-size: 0.85rem;
+  white-space: nowrap; 
 `
 
-const LikeButton = styled.button`
-  border: none;
-  position: relative;
-  top: 100%;
-  left: 90%;
-  width: 15px;
-  height: 13px;
-  z-index: 1;
-  /* opacity: 0.6; */
+const AbsolutePositionHeart = styled.div`
+  position: absolute;
+  bottom: 0.5rem;
+  right: 0.5rem;
+
+  width: max(1rem, min(5vw, 1.2rem));
+  height: max(1rem, min(5vw, 1.2rem));
   cursor: pointer;
 `
 
+const AbsolutePositionSkeleton = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+export function StoreLoadingCard() {
+  return (
+    <GridContainerGap>
+      <Frame16to10>
+        <AbsolutePositionSkeleton>
+          <Skeleton height="100%" width="100%" />
+        </AbsolutePositionSkeleton>
+      </Frame16to10>
+
+      <FlexContainerBetweenCenter>
+        <Skeleton width="40%" />
+        <FlexContainerCenter>
+          <LocationIcon />
+          <div style={{ marginLeft: '0.3rem' }}>
+            <Loading size="small" />
+          </div>
+        </FlexContainerCenter>
+      </FlexContainerBetweenCenter>
+
+      <FlexContainerGap>
+        <Skeleton borderRadius="9999px" width="5rem" height="2rem" />
+        <Skeleton borderRadius="9999px" width="5rem" height="2rem" />
+        <Skeleton borderRadius="9999px" width="5rem" height="2rem" />
+      </FlexContainerGap>
+
+      <Skeleton width="80%" />
+    </GridContainerGap>
+  )
+}
+
 type Props = {
   store: StoreCardFragment
+  afterToggleLikedStore?: (a: any) => void
   coordinates?: GeolocationCoordinates
 }
 
 function StoreCard({ store, coordinates }: Props) {
   const router = useRouter()
 
-  function goToStorePage() {
-    sessionStorage.setItem('urlBeforeStorePage', router.asPath)
-    sessionStorage.setItem('pageYOffsetBeforeStorePage', `${window.pageYOffset}`)
-    router.push(`/stores/${store.id}`)
-  }
+  const [toggleLikedStoreMutation, { loading }] = useToggleLikedStoreMutation({
+    onCompleted: (data) => {
+      console.log(data.toggleLikedStore)
+    },
+    onError: toastApolloError,
+    variables: {
+      id: store.id,
+    },
+  })
 
   useEffect(() => {
     const pageYOffset = sessionStorage.getItem('pageYOffsetBeforeStorePage')
@@ -94,45 +143,67 @@ function StoreCard({ store, coordinates }: Props) {
     }
   }, [])
 
+  function goToStorePage() {
+    sessionStorage.setItem('urlBeforeStorePage', router.asPath)
+    sessionStorage.setItem('pageYOffsetBeforeStorePage', `${window.pageYOffset}`)
+    router.push(`/stores/${store.id}`)
+  }
+
+  function toggleLikedStore(e: ReactMouseEvent<HTMLDivElement, MouseEvent>) {
+    e.stopPropagation()
+
+    if (!loading) {
+      toggleLikedStoreMutation()
+    }
+  }
+
   return (
-    <FlexContainerLi>
-      <CardImage onClick={goToStorePage}>
+    <GridContainerGap>
+      <Frame16to10 onClick={goToStorePage}>
         <Image
           src={store.imageUrls?.[0] ?? '/images/default-store-cover.png'}
           alt={store.name ?? 'store-cover'}
           layout="fill"
           objectFit="cover"
         />
-      </CardImage>
-      <div></div>
-      <CardInfo>
-        <div>{store.name}</div>
-        <div>
-          {coordinates ? (
-            formatDistance(
-              distanceBetween(
-                coordinates.latitude,
-                coordinates.longitude,
-                store.latitude,
-                store.longitude
+        <AbsolutePositionHeart onClick={toggleLikedStore}>
+          <HeartFilledIcon colored={store.isLiked} loading={loading} />
+        </AbsolutePositionHeart>
+      </Frame16to10>
+
+      <FlexContainerBetweenCenter>
+        <h4>{store.name}</h4>
+        <FlexContainerCenter>
+          <LocationIcon />
+          <h5>
+            {coordinates ? (
+              formatDistance(
+                distanceBetween(
+                  coordinates.latitude,
+                  coordinates.longitude,
+                  store.latitude,
+                  store.longitude
+                )
               )
-            )
-          ) : (
-            <Loading />
-          )}
-        </div>
-      </CardInfo>
-      <CardCategory>
+            ) : (
+              <Loading size="small" />
+            )}
+          </h5>
+        </FlexContainerCenter>
+      </FlexContainerBetweenCenter>
+
+      <FlexContainerGap>
         {store.categories.map((category, i) => (
           <CategoryButton key={i}>{category}</CategoryButton>
         ))}
-      </CardCategory>
-      <CardTags>
+      </FlexContainerGap>
+
+      <FlexContainerGap>
         {store.hashtags?.map((hashtag, i) => (
           <Tag key={i}>#{hashtag} </Tag>
         ))}
-      </CardTags>
-    </FlexContainerLi>
+      </FlexContainerGap>
+    </GridContainerGap>
   )
 }
 
