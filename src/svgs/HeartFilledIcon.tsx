16c9f39b9e061784b798e27657eae9154526c05d
Taeuk Gwak <gwak2837@kakao.com>
2021-10-28 21:53:30 +0900

    Update HeartFilledIcon props

--- src/svgs/HeartFilledIcon.tsx
+++ src/svgs/HeartFilledIcon.tsx
@@ -17,16 +17,23 @@ const Path = styled.path`
 `
 
 type Props = {
-  filled?: boolean
+  colored?: boolean
+  loading?: boolean
 }
 
-function HeartFilledIcon({ filled }: Props) {
+function HeartFilledIcon({ colored, loading }: Props) {
+  const fillingColor = loading
+    ? 'rgba(255, 255, 255, .9)'
+    : colored
+    ? 'rgba(255, 159, 116, .9)'
+    : 'rgba(255, 255, 255, .5)'
+
   return (
     <Svg width="100%" height="100%" viewBox="0 0 21.685 18.896">
       <Path
         d="M10.845 18.496C21.406 12.809 21.323 6.687 21.273 5.952c0-.025.008-.048.008-.074A5.409 5.409 0 0010.84 3.826 5.408 5.408 0 00.401 5.878c0 .03.008.057.009.086-.05.783-.076 6.871 10.435 12.532z"
-        fill={`rgba(255,255,255,.${filled ? '9' : '5'})`}
-        stroke="#e8e8e8"
+        fill={fillingColor}
+        stroke={colored ? '#ff9f74' : '#e8e8e8'}
       />
     </Svg>
   )
